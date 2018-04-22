3f0a8dd6ec574af29c471c27e739ad083ee14e94
Chris Mohr <chris.pintz.mohr@gmail.com>
2018-04-22 17:27:10 -0400

    Display static pack from API call

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: props.data.url,
+      name: props.data.name
+    }
+  }
+
+  render() {
+    return (
+      <img
+        className="card-image"
+        src={this.state.url}
+        alt={this.state.name}
+        title={this.state.name}
+      />
+    )
+  }
+}
+
+class CardRow extends Component {
+  render() {
+    return (
+      <div className="card-row">
+        {this.props.data.map((card_data) =>
+          <Card key={card_data.name} data={card_data} />)}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   state = {
-    response: ''
+    response: null 
   };
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/api/static_pack');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -27,9 +59,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Grid Draft</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        { this.state.response != null &&
+          <div className="draft">
+            <CardRow data={this.state.response.row_1} />
+            <CardRow data={this.state.response.row_2} />
+            <CardRow data={this.state.response.row_3} />
+          </div>
+        }
       </div>
     );
   }
--- server.js
+++ server.js
@@ -7,4 +7,54 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
+const static_pack = {
+  row_1: [
+    {
+      name: "Snapcaster Mage",
+      url: "https://img.scryfall.com/cards/normal/en/mm3/50.jpg?1517813031"
+    },
+    {
+      name: "Fyndhorn Elves",
+      url: "https://img.scryfall.com/cards/normal/en/vma/210.jpg?1517813031"
+    },
+    {
+      name: "Shriekmaw",
+      url: "https://img.scryfall.com/cards/normal/en/cma/68.jpg?1519868787"
+    }
+  ],
+  row_2: [
+    {
+      name: "Strip Mine",
+      url: "https://img.scryfall.com/cards/normal/en/vma/316.jpg?1517813031"
+    },
+    {
+      name: "Baleful Strix",
+      url: "https://img.scryfall.com/cards/normal/en/e01/80.jpg?1517813031"
+    },
+    {
+      name: "Snuff Out",
+      url: "https://img.scryfall.com/cards/normal/en/gvl/53.jpg?1520561603"
+    }
+  ],
+  row_3: [
+    {
+      name: "Maelstrom Pulse",
+      url: "https://img.scryfall.com/cards/normal/en/mma/180.jpg?1517813031"
+    },
+    {
+      name: "Looter il-Kor",
+      url: "https://img.scryfall.com/cards/normal/en/td0/A25.jpg?1517813031"
+    },
+    {
+      name: "Goblin Rabblemaster",
+      url: "https://img.scryfall.com/cards/normal/en/ddt/46.jpg?1517813031"
+    }
+  ]
+}
+
+
+app.get('/api/static_pack', (req, res) => {
+  res.send(static_pack);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));

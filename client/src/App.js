2b841277d3f3062626aba34426aa8b4b6cf8a561
Chris Mohr <chris.pintz.mohr@gmail.com>
2018-04-23 00:44:57 -0400

    Remove animated logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class Card extends Component {
@@ -79,7 +78,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Grid Draft</h1>
         </header>
         { this.state.response != null &&
